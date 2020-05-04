49868b89cf5ed6e395efaf2575aab26a2a522c56
FreezingKas <maxence.jung1@gmail.com>
2020-05-04 19:53:46 +0200

    separation

--- cogs/teacher_command.py
+++ cogs/teacher_command.py
@@ -1,7 +1,13 @@
 import discord
-from discord import FFmpegPCMAudio
 from discord.ext import commands
-from discord.utils import get
 import os
 
 
+class Teacher(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+
+def setup(client):
+    client.add_cog(Teacher(client))
--- cogs/utils_command.py
+++ cogs/utils_command.py
@@ -1,5 +1,13 @@
 import discord
-from discord import FFmpegPCMAudio
 from discord.ext import commands
-from discord.utils import get
 import os
+
+
+class Utils(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+
+def setup(client):
+    client.add_cog(Utils(client))
