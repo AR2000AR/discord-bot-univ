bb8a3ab228ac0d4c647769f83d63ff65295b0c73
FreezingKas <maxence.jung1@gmail.com>
2020-09-01 12:49:45 +0200

    Update bot_command.py

--- bot_command.py
+++ bot_command.py
@@ -29,7 +29,7 @@ async def on_ready():
     for guild in client.guilds:
         print(f'{client.user} has connected   to' f' {guild.name} id: {guild.id}')
 
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie"))
+    await client.change_presence(activity=discord.CustomActivity(type=discord.ActivityType.custom, name="a movie"))
 
 
 
