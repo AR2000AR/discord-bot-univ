1c1e3a40268e1b43ff8cc53a9d6d54251ac29c95
FreezingKas <maxence.jung1@gmail.com>
2020-09-11 13:42:26 +0200

    Update teacher_command.py

--- cogs/teacher_command.py
+++ cogs/teacher_command.py
@@ -9,6 +9,91 @@ class Teacher(commands.Cog):
         self.client = client
 
     @commands.command()
+    async def appelabs(self, ctx, *args):
+        try:
+            channel = ctx.message.author.voice.channel
+        except AttributeError:
+            await ctx.send("Tu n'es pas dans un salon vocal !")
+            return
+
+            
+        allowed_role = [] # array contenant tous les rôles du serveur pouvant éxécuter la commande
+        # On fait la liste des rôles autorisés à utiliser cette commande
+        for role in ctx.guild.roles:
+            if role.name.startswith("Enseignant") or role.name.startswith("Admin"):
+                allowed_role.append(role)
+
+        # On vérifie que l'utilisateur a le droit d'utiliser la commande
+        allowedUser = False
+
+        for role in ctx.author.roles:
+            if role in allowed_role:
+                allowedUser = True
+                break
+        
+        if not allowedUser:
+            await ctx.send("Vous n'êtes pas autorisé à utiliser cette commande !")
+            return
+
+        # Initialisation des variables
+        server_role = [] # array contenant tous les rôles du serveur
+        list_group = [] # array contenant tous les membres ayant les rôles dont on veut faire l'appel
+        list_connected = [] # array contenant tous les mebres connectés au serveur vocal dont on vaut faire l'appel
+
+        
+        # On ajoute les roles présent sur le serveur dans une lsite
+        for role in ctx.guild.roles:
+            server_role.append(role.name)
+        
+        # On check si les rôles passés en arguments existent sur le serveur
+        for arg in args:
+            if arg not in server_role:
+                await ctx.send("Un des rôles en paramètres n'existe pas sur ce serveur !")
+        
+        # On check si l'auteur du message est bien connecté à un salon vocal
+        try:
+            channel = ctx.message.author.voice.channel
+        except AttributeError:
+            await ctx.send("Tu n'es pas dans un salon vocal !")
+            return
+        
+        # Message de requête
+        await ctx.send(f"Appel dans le salon vocal {channel.name}")
+        print("Requête d'appel via !appel")
+        
+
+
+        # On récupère tous les membres d'un groupe
+        countRoles = len(args)
+        for member in ctx.guild.members:
+            countMem = 0
+            for role in member.roles:
+                if role.name in args:
+                    countMem += 1
+            if countMem == countRoles:
+                list_group.append(member)
+
+
+        # On récupère tous les membres connecté au channel
+        for eleve in channel.members:
+            list_connected.append(eleve.name)
+
+        # décalaration des variables utilisés pour crééer les message
+        i = 0
+        appel_message = ""
+        # ajoute la présence de la personne dans le message qui va être envoyé
+        for present in list_group:
+            if present.name not in list_connected:
+                appel_message += f"`{present.nick} ({present.name})` est présent :red_circle:\n"
+            
+
+                i += 1
+
+        # envoi du message
+        await ctx.send(appel_message + "\n" + f"Il y a {i} absent(s).")
+
+    
+    @commands.command()
     async def appel(self, ctx, *args):
 
         try:
